68dc116e00ef25b0c21837737a0f75133d236d1b
pauloandredm <pauloadmendonca@gmail.com>
2024-03-09 19:48:02 -0300

    allow all

--- data_analise/settings.py
+++ data_analise/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-82h=5ly5#-%z$0xr)&(nbw$*^e$!^prv9din$&ik)t^0(2u_(2
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = ['ec2-18-228-153-71.sa-east-1.compute.amazonaws.com',
-                  '127.0.0.1'
+                  '127.0.0.1',
+                  '*'
 ]
 
 
